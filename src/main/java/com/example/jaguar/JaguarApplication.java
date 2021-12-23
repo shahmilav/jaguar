2d253a460e51dcd81152ec1508ce0d1a542a856a
Milav Shah <shahmilav@gmail.com>
2021-12-23 18:36:19 -0500

    first commit

---
+++ src/main/java/com/example/jaguar/JaguarApplication.java
@@ -0,0 +1,40 @@
+package com.example.jaguar;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JaguarApplication {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaguarApplication.class, args);
+    }
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+    
+    @PostMapping("/login")
+    public String authenticate(@RequestParam(name = "email") String username, @RequestParam(name = "password") String password, Model model) {
+        System.out.println(username);
+        System.out.println(password);
+        model.addAttribute("name", username);
+        return "dashboard";
+    }
+}
