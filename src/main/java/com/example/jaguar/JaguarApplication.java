a4afa4863eb1b86189d335bcd75980fa830d779f
Milav Shah <shahmilav@gmail.com>
2021-12-23 21:04:46 -0500

    commit

--- src/main/java/com/example/jaguar/JaguarApplication.java
+++ src/main/java/com/example/jaguar/JaguarApplication.java
@@ -3,6 +3,7 @@ package com.example.jaguar;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class JaguarApplication {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+
+    private final static String validUsername = "me@milav.com";
+    private final static String validPassword = "milav";
+    private boolean isUserLoggedIn = false;
 
     public static void main(String[] args) {
         SpringApplication.run(JaguarApplication.class, args);
@@ -29,12 +34,35 @@ public class JaguarApplication {
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
-    
-    @PostMapping("/login")
-    public String authenticate(@RequestParam(name = "email") String username, @RequestParam(name = "password") String password, Model model) {
+
+    @PostMapping("/dashboard")
+    public String authenticate(@RequestParam(name = "email") String username,
+            @RequestParam(name = "password") String password, Model model) {
         System.out.println(username);
         System.out.println(password);
-        model.addAttribute("name", username);
-        return "dashboard";
+
+        if ((username.equalsIgnoreCase(validUsername)) && (password.equals(validPassword))) {
+            isUserLoggedIn = true;
+            model.addAttribute("name", "Welcome to Jaguar Dashboard, " + username);
+            return "dashboard";
+        } else {
+            return "index";
+        }
+    }
+
+    @GetMapping("/dashboard")
+    public String validateUser(@RequestParam(name = "email") String username,
+            @RequestParam(name = "password") String password, Model model) {
+        if (isUserLoggedIn) {
+            return "dashboard";
+        } else {
+            return "index";
+        }
+    }
+
+    @GetMapping(value = {"/", "/index"})
+    public String index(Model model) {
+        model.addAttribute("title", "Login Page");
+        return "index";
     }
 }
