1028aa54979d389c47eba3144c2b94c35ae20235
Milav Shah <shahmilav@gmail.com>
2021-12-24 21:20:07 -0500

    Fixed the url issue, began with mongoDB integration.

--- src/main/java/com/example/jaguar/JaguarApplication.java
+++ src/main/java/com/example/jaguar/JaguarApplication.java
@@ -1,5 +1,6 @@
 package com.example.jaguar;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -15,13 +16,19 @@ public class JaguarApplication {
     private final static String validUsername = "me@milav.com";
     private final static String validPassword = "milav";
     private boolean isUserLoggedIn = false;
+    @Autowired
+    private static UserRepository repository;
 
     public static void main(String[] args) {
+        createUser();
         SpringApplication.run(JaguarApplication.class, args);
     }
 
-    
-    @PostMapping("/dashboard")
+    private static void createUser() {
+        repository.save(new User("me@milav.com", "my-secret-password"));
+    }
+
+    @PostMapping("/login")
     public String authenticate(@RequestParam(name = "email") String username,
             @RequestParam(name = "password") String password, Model model) {
         System.out.println(username);
@@ -29,24 +36,23 @@ public class JaguarApplication {
 
         if ((username.equalsIgnoreCase(validUsername)) && (password.equals(validPassword))) {
             isUserLoggedIn = true;
-            model.addAttribute("name", "Welcome to Jaguar Dashboard, " + username);
-            return "dashboard";
+            return "redirect:/dashboard";
         } else {
             return "index";
         }
     }
 
     @GetMapping("/dashboard")
-    public String validateUser(@RequestParam(name = "email") String username,
-            @RequestParam(name = "password") String password, Model model) {
+    public String validateUser(Model model) {
         if (isUserLoggedIn) {
+            model.addAttribute("name", "Welcome to Jaguar Dashboard, " + validUsername);
             return "dashboard";
         } else {
-            return "index";
+            return "redirect:/index";
         }
     }
 
-    @GetMapping(value = { "/", "/index" })
+    @GetMapping(value = {"/", "/index"})
     public String index(Model model) {
         model.addAttribute("title", "Login Page");
         return "index";
---
+++ src/main/java/com/example/jaguar/User.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.jaguar;
+
+/**
+ *
+ * @author milavshah
+ */
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+
+    @Id
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+    
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[email=%s, firstName='%s', lastName='%s']",
+                email, firstName, lastName);
+    }
+}
