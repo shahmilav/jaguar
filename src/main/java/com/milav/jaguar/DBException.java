eabac1f8e6adc73bd1787806edbd751f36920eb6
shahmilav <53659750+shahmilav@users.noreply.github.com>
2021-12-27 11:49:50 -0500

    Fixed MongoDB nullpointerexception by manually connecting to MongoDB.

---
+++ src/main/java/com/milav/jaguar/DBException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.milav.jaguar;
+
+/**
+ *
+ * @author jigarshah
+ */
+public class DBException extends Exception {
+    
+    public DBException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
--- src/main/java/com/milav/jaguar/UserRepository.java
+++ src/main/java/com/milav/jaguar/UserRepository.java
@@ -8,9 +8,7 @@ package com.milav.jaguar;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
   public User findByEmail(String email);
