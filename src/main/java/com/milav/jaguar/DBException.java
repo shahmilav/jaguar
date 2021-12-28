80fadccfa0434f8679da1b2f0983f089831b2a6e
shahmilav <53659750+shahmilav@users.noreply.github.com>
2021-12-28 11:42:07 -0500

    Fixed dashboard, began working on signup.

--- src/main/java/com/milav/jaguar/DBException.java
+++ src/main/java/com/milav/jaguar/DBException.java
@@ -1,17 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.milav.jaguar;
 
-/**
- *
- * @author jigarshah
- */
 public class DBException extends Exception {
-    
+
     public DBException(String message, Throwable cause) {
         super(message, cause);
     }
-    
+
 }
--- src/main/java/com/milav/jaguar/JaguarApplication.java
+++ src/main/java/com/milav/jaguar/JaguarApplication.java
@@ -1,11 +1,5 @@
 package com.milav.jaguar;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import java.util.Date;
-import java.util.HashMap;
-import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,48 +13,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class JaguarApplication {
 
+    // @Autowired
+    // private CreateUser createUser;
     private final static String validUsername = "me@milav.com";
     private final static String validPassword = "milav";
     private boolean isUserLoggedIn = false;
 
     @Autowired
-    private UserRepository repository;
-
     public static void main(String[] args) throws DBException {
-        JaguarApplication app = new JaguarApplication();
-        app.createUserInDB();
-        
-        //app.createUser();
+
         SpringApplication.run(JaguarApplication.class, args);
-    }
-
-    private void createUser() {
-        repository.save(new User("me@milav.com", "my-secret-password"));
-    }
-
-    private void createUserInDB() throws DBException {
-
-        MongoDatabase db = DBManager.getMongoDB();
-
-        if (db.getCollection("USER_PROFILE") == null) {
-            db.createCollection("USER_PROFILE");
-        }
-
-        MongoCollection coll = db.getCollection("USER_PROFILE");
-
-        Document doc = new Document();
-        doc.put("firstName", "Milav");
-        doc.put("lastName", "Shah");
-        doc.put("email", "me@milav.com");
-        doc.put("password", "mysecretpassword");
-
-        Date date = new Date();
-        doc.put("createDateTime", date);
-        doc.put("updateDateTime", date);
-        doc.put("lastLoginDateTime", date);
-
-        coll.insertOne(doc);
-        coll.createIndex(new BasicDBObject("email", 1));
     }
 
     @PostMapping("/login")
@@ -87,9 +49,26 @@ public class JaguarApplication {
         }
     }
 
-    @GetMapping(value = {"/", "/index"})
+    @GetMapping(value = { "/", "/index" })
     public String index(Model model) {
         model.addAttribute("title", "Login Page");
         return "index";
+    }
+
+    @PostMapping("/register")
+    public String signUp(
+            @RequestParam(name = "fname") String firstName,
+            @RequestParam(name = "lname") String lastName,
+            @RequestParam(name = "email") String username,
+            @RequestParam(name = "password") String password,
+            Model model) {
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(username);
+        System.out.println(password);
+
+        return "redirect:/login";
+
     }
 }
