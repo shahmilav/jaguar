1674d288f8f98434bdcd6768f5104348237fc8ad
shahmilav <53659750+shahmilav@users.noreply.github.com>
2022-01-14 06:45:25 -0500

    Fixed selenium.

--- src/main/java/com/milav/jaguar/application/selenium/SeleniumTest.java
+++ src/main/java/com/milav/jaguar/application/selenium/SeleniumTest.java
@@ -1,20 +1,26 @@
 package com.milav.jaguar.application.selenium;
 
+import com.milav.jaguar.application.app.JaguarApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class SeleniumTest {
-    public WebDriver driver;
+    private static final Logger LOGGER = LogManager.getLogger(JaguarApplication.class);
+
 
     /**
      * The method runs the Selenium web driver on Firefox.
      */
     public void runSelenium() {
+        LOGGER.info("Entering runSelenium method");
 
         FirefoxOptions options = new FirefoxOptions();
-        driver = new FirefoxDriver(options);
 
+        WebDriver driver = new FirefoxDriver(options);
+        LOGGER.info("Running selenium.");
         driver.quit();
     }
 }
