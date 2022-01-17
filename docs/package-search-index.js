499024ed8fcbfdc2ee4018d87934e71cac7da10e
Milav Shah <53659750+shahmilav@users.noreply.github.com>
2022-01-16 19:41:56 -0500

    Delete docs directory

--- docs/package-search-index.js
+++
@@ -1,5 +0,0 @@
-packageSearchIndex = [{
-    "l": "All Packages",
-    "u": "allpackages-index.html"
-}, {"l": "com.milav.jaguar.account"}, {"l": "com.milav.jaguar.application.app"}, {"l": "com.milav.jaguar.application.controller"}, {"l": "com.milav.jaguar.application.selenium"}, {"l": "com.milav.jaguar.auth.login"}, {"l": "com.milav.jaguar.auth.logout"}, {"l": "com.milav.jaguar.auth.signup"}, {"l": "com.milav.jaguar.database.errors"}, {"l": "com.milav.jaguar.database.manager"}, {"l": "com.milav.jaguar.user"}, {"l": "com.milav.jaguar.user.util"}, {"l": "com.milav.jaguar.utils"}];
-updateSearchResults();
