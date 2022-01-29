0cc290d14b0fcbb0dd74eac320ab2163bb34db01
shahmilav <53659750+shahmilav@users.noreply.github.com>
2022-01-28 21:59:01 -0500

    Updated auth files, as they got messed up.

--- src/main/java/com/milav/jaguar/auth/errors/PasswordGenException.java
+++ src/main/java/com/milav/jaguar/auth/errors/PasswordGenException.java
@@ -2,14 +2,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.pebblenotes.auth.errors;
+package com.milav.jaguar.auth.errors;
 
 /**
- *
  * @author jigarshah
  */
 public class PasswordGenException extends Exception {
-    
+
     public PasswordGenException(String message, Throwable cause) {
         super(message, cause);
     }
