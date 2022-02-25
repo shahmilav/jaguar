e47d45549bba2cd61bc8e62ee707d9259d9f5e48
shahmilav <53659750+shahmilav@users.noreply.github.com>
2022-02-24 22:36:05 -0500

    Formatted code

--- src/main/java/com/milav/jaguar/auth/errors/PasswordGenException.java
+++ src/main/java/com/milav/jaguar/auth/errors/PasswordGenException.java
@@ -1,6 +1,8 @@
 package com.milav.jaguar.auth.errors;
 
-/** @author Jigar Shah */
+/**
+ * @author Jigar Shah
+ */
 public class PasswordGenException extends Exception {
 
   public PasswordGenException(String message, Throwable cause) {
