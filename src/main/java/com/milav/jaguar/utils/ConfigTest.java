7c112af1768d2712c6b17bec324dcdeabffbfcea
shahmilav <53659750+shahmilav@users.noreply.github.com>
2022-04-23 15:06:10 -0400

    ConfigTest

--- src/main/java/com/milav/jaguar/utils/ConfigTest.java
+++ src/main/java/com/milav/jaguar/utils/ConfigTest.java
@@ -1,2 +1,21 @@
-package com.milav.jaguar.utils;public class ConfigTest {
+package com.milav.jaguar.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class ConfigTest {
+
+  //   TODO: why is this value null? fix this
+  /*  @Value("${mongodb.uri}")
+  static String dbURI;*/
+
+  @Value("${mongodb.database}")
+  String dbName;
+
+  public ConfigTest(@Value("${mongodb.uri}") String dbURI) {
+    // this is a constructor, it should work?
+  }
+
+  public static void main(String[] args) {
+    /* TODO document why this method is empty */
+  }
 }
