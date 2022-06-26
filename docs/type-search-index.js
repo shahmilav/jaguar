66712ce3d47eb613c7838b13493c2f2780bbb221
shahmilav <53659750+shahmilav@users.noreply.github.com>
2022-06-26 14:41:49 -0400

    Regenerate docs

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,26 @@
-typeSearchIndex = [{"p":"com.milav.jaguar.account","l":"AccountController"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.milav.jaguar.auth.util","l":"AuthUtil"},{"p":"com.milav.jaguar.utils","l":"ConfigTest"},{"p":"com.milav.jaguar.database.errors","l":"DBException"},{"p":"com.milav.jaguar.database.manager","l":"DBManager"},{"p":"com.milav.jaguar.application.app","l":"JaguarApplication"},{"p":"com.milav.jaguar.application.controller","l":"JaguarController"},{"p":"com.milav.jaguar.utils","l":"JaguarUtils"},{"p":"com.milav.jaguar.auth.login","l":"LoginController"},{"p":"com.milav.jaguar.auth.logout","l":"LogoutController"},{"p":"com.milav.jaguar.auth.errors","l":"PasswordGenException"},{"p":"com.milav.jaguar.account","l":"ProfileController"},{"p":"com.milav.jaguar.application.selenium","l":"SeleniumTest"},{"p":"com.milav.jaguar.auth.signup","l":"SignupController"},{"p":"com.milav.jaguar.user","l":"User"},{"p":"com.milav.jaguar.user","l":"UserController"},{"p":"com.milav.jaguar.user.util","l":"UserUtil"}];updateSearchResults();
+typeSearchIndex = [{"p": "com.milav.jaguar.account", "l": "AccountController"}, {
+    "l": "All Classes and Interfaces",
+    "u": "allclasses-index.html"
+}, {"p": "com.milav.jaguar.auth.util", "l": "AuthUtil"}, {
+    "p": "com.milav.jaguar.utils",
+    "l": "ConfigTest"
+}, {"p": "com.milav.jaguar.database.errors", "l": "DBException"}, {
+    "p": "com.milav.jaguar.database.manager",
+    "l": "DBManager"
+}, {"p": "com.milav.jaguar.application.app", "l": "JaguarApplication"}, {
+    "p": "com.milav.jaguar.application.controller",
+    "l": "JaguarController"
+}, {"p": "com.milav.jaguar.utils", "l": "JaguarUtils"}, {
+    "p": "com.milav.jaguar.auth.login",
+    "l": "LoginController"
+}, {"p": "com.milav.jaguar.auth.logout", "l": "LogoutController"}, {
+    "p": "com.milav.jaguar.auth.errors",
+    "l": "PasswordGenException"
+}, {"p": "com.milav.jaguar.account", "l": "ProfileController"}, {
+    "p": "com.milav.jaguar.application.selenium",
+    "l": "SeleniumTest"
+}, {"p": "com.milav.jaguar.auth.signup", "l": "SignupController"}, {
+    "p": "com.milav.jaguar.user",
+    "l": "User"
+}, {"p": "com.milav.jaguar.user", "l": "UserController"}, {"p": "com.milav.jaguar.user.util", "l": "UserUtil"}];
+updateSearchResults();
