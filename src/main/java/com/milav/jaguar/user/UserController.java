c3523019ff9ce9cc63c3a5ab6bc704ff76902849
shahmilav <53659750+shahmilav@users.noreply.github.com>
2022-06-26 14:43:50 -0400

    Whitespace

--- src/main/java/com/milav/jaguar/user/UserController.java
+++ src/main/java/com/milav/jaguar/user/UserController.java
@@ -28,193 +28,215 @@ import static com.mongodb.client.model.Filters.eq;
 @Service
 public class UserController {
 
-  /** Logger. */
-  private static final Logger LOGGER = LogManager.getLogger(UserController.class);
-  /** User related utilities. */
-  private final UserUtil userUtil = new UserUtil();
-  /** Generic database connection error. */
-  String dbError = "Error accessing the database.";
-  /** The name of the MongoDB collection in the database. */
-  String collectionName = "USER_PROFILE";
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(UserController.class);
+    /**
+     * User related utilities.
+     */
+    private final UserUtil userUtil = new UserUtil();
+    /**
+     * Generic database connection error.
+     */
+    String dbError = "Error accessing the database.";
+    /**
+     * The name of the MongoDB collection in the database.
+     */
+    String collectionName = "USER_PROFILE";
 
-  // Field Names
+    // Field Names
 
-  /** First name field. */
-  String firstNameField = "firstName";
-  /** Last name field. */
-  String lastNameField = "lastName";
-  /** Email field. */
-  String emailField = "email";
-  /** Password field. */
-  String passwordField = "password";
-  /** Salt field. */
-  String saltField = "salt";
+    /**
+     * First name field.
+     */
+    String firstNameField = "firstName";
+    /**
+     * Last name field.
+     */
+    String lastNameField = "lastName";
+    /**
+     * Email field.
+     */
+    String emailField = "email";
+    /**
+     * Password field.
+     */
+    String passwordField = "password";
+    /**
+     * Salt field.
+     */
+    String saltField = "salt";
 
-  /**
-   * The method creates an entry in the database with the given information.
-   *
-   * @param firstName first name of the user.
-   * @param lastName last name of the user.
-   * @param email email of the user.
-   * @param password user's password.
-   * @param salt sprinkle the salt to make password guessing hard
-   */
-  public void createUserInDB(
-      String firstName,
-      String lastName,
-      @NotNull String email,
-      @NotNull String password,
-      @NotNull String salt) {
+    DBManager dbManager = new DBManager();
 
-    LOGGER.info("Entering createUserInDB method");
-    MongoDatabase db = DBManager.getMongoDB();
+    /**
+     * The method creates an entry in the database with the given information.
+     *
+     * @param firstName first name of the user.
+     * @param lastName  last name of the user.
+     * @param email     email of the user.
+     * @param password  user's password.
+     * @param salt      sprinkle the salt to make password guessing hard
+     */
+    public void createUserInDB(
+            String firstName,
+            String lastName,
+            @NotNull String email,
+            @NotNull String password,
+            @NotNull String salt) {
 
-    // get database collection.
-    MongoCollection<Document> collection = db.getCollection(collectionName);
+        LOGGER.info("Entering createUserInDB method");
+        MongoDatabase db = dbManager.getMongoDB();
 
-    // put user credentials into the document.
-    Document document = new Document();
-    document.put(firstNameField, firstName);
-    document.put(lastNameField, lastName);
-    document.put(emailField, email.toLowerCase());
-    document.put(passwordField, password);
-    document.put(saltField, salt);
+        // get database collection.
+        MongoCollection<Document> collection = db.getCollection(collectionName);
 
-    // place the date in the document.
-    Date date = new Date();
-    document.put("createDateTime", date);
-    document.put("updateDateTime", date);
-    document.put("lastLoginDateTime", date);
+        // put user credentials into the document.
+        Document document = new Document();
+        document.put(firstNameField, firstName);
+        document.put(lastNameField, lastName);
+        document.put(emailField, email.toLowerCase());
+        document.put(passwordField, password);
+        document.put(saltField, salt);
 
-    // insert documents into the database.
-    collection.insertOne(document);
-    collection.createIndex(new BasicDBObject(emailField, 1));
-    LOGGER.info("User created in DB");
-  }
+        // place the date in the document.
+        Date date = new Date();
+        document.put("createDateTime", date);
+        document.put("updateDateTime", date);
+        document.put("lastLoginDateTime", date);
 
-  /**
-   * The method finds a user given an email. It is assumed that each user has a unique email
-   * address. You cannot have multiple users with the same email address.
-   *
-   * @param email the search query
-   * @return boolean
-   */
-  public boolean doesUserExist(@NotNull String email) {
-
-    MongoDatabase db = DBManager.getMongoDB();
-    Document document = new Document();
-    document.put(emailField, email.toLowerCase());
-    MongoCollection<Document> collection = db.getCollection(collectionName);
-    return (collection.find(document).first() != null);
-  }
-
-  /**
-   * The method finds a user given an email address.
-   *
-   * <p>It returns a User object with prefilled data retrieved from the database. The user can be
-   * null if the user does not exist in the database.
-   *
-   * @param email the search query to match against the database.
-   * @return User
-   * @throws DBException we have to connect to the database to find the user.
-   */
-  public User findUser(@NotNull String email) throws DBException {
-
-    MongoDatabase db;
-
-    db = DBManager.getMongoDB();
-
-    Document document = new Document();
-    document.put(emailField, email.toLowerCase());
-    MongoCollection<Document> collection = db.getCollection(collectionName);
-    FindIterable<Document> findIterable = collection.find(document);
-    Document result;
-
-    try {
-      result = findIterable.first();
-    } catch (MongoTimeoutException timeoutException) {
-      throw new DBException(dbError, timeoutException);
+        // insert documents into the database.
+        collection.insertOne(document);
+        collection.createIndex(new BasicDBObject(emailField, 1));
+        LOGGER.info("User created in DB");
     }
 
-    if (result != null) {
+    /**
+     * The method finds a user given an email. It is assumed that each user has a
+     * unique email
+     * address. You cannot have multiple users with the same email address.
+     *
+     * @param email the search query
+     * @return boolean
+     */
+    public boolean doesUserExist(@NotNull String email) {
 
-      LOGGER.info("User found!");
-      return new User(
-          email,
-          result.getString(passwordField),
-          result.getString(saltField),
-          result.getString(firstNameField),
-          result.getString(lastNameField));
-
-    } else {
-      LOGGER.info("User not found");
-      return null;
+        MongoDatabase db = dbManager.getMongoDB();
+        Document document = new Document();
+        document.put(emailField, email.toLowerCase());
+        MongoCollection<Document> collection = db.getCollection(collectionName);
+        return (collection.find(document).first() != null);
     }
-  }
 
-  /**
-   * The method finds a user in the database and edits the entry.
-   *
-   * @param oldInfo the user's old information
-   * @param firstname the user's new first name
-   * @param lastname the user's new last name
-   * @param email the user's new email
-   * @param password the user's new password
-   * @return User
-   */
-  public User updateUserInDB(
-      @NotNull User oldInfo,
-      String firstname,
-      String lastname,
-      String email,
-      String password,
-      String salt) {
-    MongoDatabase db;
+    /**
+     * The method finds a user given an email address.
+     *
+     * <p>It returns a User object with prefilled data retrieved from the database. The user can be
+     * null if the user does not exist in the database.
+     *
+     * @param email the search query to match against the database.
+     * @return User
+     * @throws DBException we have to connect to the database to find the user.
+     */
+    public User findUser(@NotNull String email) throws DBException {
 
-    db = DBManager.getMongoDB();
+        MongoDatabase db;
 
-    MongoCollection<Document> collection = db.getCollection(collectionName);
+        db = dbManager.getMongoDB();
 
-    BasicDBObject searchQuery = new BasicDBObject().append(emailField, oldInfo.getEmail());
-    BasicDBObject newDocument = new BasicDBObject();
+        Document document = new Document();
+        document.put(emailField, email.toLowerCase());
+        MongoCollection<Document> collection = db.getCollection(collectionName);
+        FindIterable<Document> findIterable = collection.find(document);
+        Document result;
 
-    userUtil.appendDoc("$set", newDocument, firstNameField, firstname);
+        try {
+            result = findIterable.first();
+        } catch (MongoTimeoutException timeoutException) {
+            throw new DBException(dbError, timeoutException);
+        }
 
-    collection.updateOne(searchQuery, newDocument);
-    LOGGER.info(newDocument);
+        if (result != null) {
 
-    userUtil.appendDoc("$set", newDocument, lastNameField, lastname);
+            LOGGER.info("User found!");
+            return new User(
+                    email,
+                    result.getString(passwordField),
+                    result.getString(saltField),
+                    result.getString(firstNameField),
+                    result.getString(lastNameField));
 
-    collection.updateOne(searchQuery, newDocument);
-    LOGGER.info(newDocument);
+        } else {
+            LOGGER.info("User not found");
+            return null;
+        }
+    }
 
-    userUtil.appendDoc("$set", newDocument, passwordField, password);
-    collection.updateOne(searchQuery, newDocument);
 
-    userUtil.appendDoc("$set", newDocument, saltField, salt);
-    collection.updateOne(searchQuery, newDocument);
+    /**
+     * The method finds a user in the database and edits the entry.
+     *
+     * @param oldInfo   the user's old information
+     * @param firstname the user's new first name
+     * @param lastname  the user's new last name
+     * @param email     the user's new email
+     * @param password  the user's new password
+     * @return User
+     */
+    public User updateUserInDB(
+            @NotNull User oldInfo,
+            String firstname,
+            String lastname,
+            String email,
+            String password,
+            String salt) {
+        MongoDatabase db;
 
-    userUtil.appendDoc("$set", newDocument, emailField, email);
-    collection.updateOne(searchQuery, newDocument);
+        db = dbManager.getMongoDB();
 
-    return userUtil.fillUpUser(firstname, lastname, email, password);
-  }
+        MongoCollection<Document> collection = db.getCollection(collectionName);
 
-  /**
-   * Deletes the user from the database.
-   *
-   * @param email the search query.
-   */
-  public void deleteUserFromDB(String email) {
+        BasicDBObject searchQuery = new BasicDBObject().append(emailField, oldInfo.getEmail());
+        BasicDBObject newDocument = new BasicDBObject();
 
-    LOGGER.info("Entering deleteUserFromDB");
-    MongoDatabase db;
+        userUtil.appendDoc("$set", newDocument, firstNameField, firstname);
 
-    db = DBManager.getMongoDB();
+        collection.updateOne(searchQuery, newDocument);
+        LOGGER.info(newDocument);
 
-    MongoCollection<Document> collection = db.getCollection(collectionName);
+        userUtil.appendDoc("$set", newDocument, lastNameField, lastname);
 
-    Bson query = eq("email", email);
-    collection.deleteOne(query);
-  }
+        collection.updateOne(searchQuery, newDocument);
+        LOGGER.info(newDocument);
+
+        userUtil.appendDoc("$set", newDocument, passwordField, password);
+        collection.updateOne(searchQuery, newDocument);
+
+        userUtil.appendDoc("$set", newDocument, saltField, salt);
+        collection.updateOne(searchQuery, newDocument);
+
+        userUtil.appendDoc("$set", newDocument, emailField, email);
+        collection.updateOne(searchQuery, newDocument);
+
+        return userUtil.fillUpUser(firstname, lastname, email, password);
+    }
+
+    /**
+     * Deletes the user from the database.
+     *
+     * @param email the search query.
+     */
+    public void deleteUserFromDB(String email) {
+
+        LOGGER.info("Entering deleteUserFromDB");
+        MongoDatabase db;
+
+        db = dbManager.getMongoDB();
+
+        MongoCollection<Document> collection = db.getCollection(collectionName);
+
+        Bson query = eq("email", email);
+        collection.deleteOne(query);
+    }
 }
