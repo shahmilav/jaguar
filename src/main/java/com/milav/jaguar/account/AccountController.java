2cf8f97ad74bb35518dc15b05e58478abd46e7c9
Milav Shah <53659750+shahmilav@users.noreply.github.com>
2022-08-02 20:21:51 -0400

    Whitespace

--- src/main/java/com/milav/jaguar/account/AccountController.java
+++ src/main/java/com/milav/jaguar/account/AccountController.java
@@ -56,15 +56,7 @@ public class AccountController {
      * @throws DBException since we are connecting to the database
      */
     @GetMapping("/savechanges")
-    public String saveChanges(
-            @RequestParam("firstname") String firstname,
-            @RequestParam("lastname") String lastname,
-            @RequestParam("currentPassword") String currentPassword,
-            @RequestParam("email") String newEmail,
-            @RequestParam("newPassword") String newPassword,
-            Model model,
-            @NotNull HttpSession session)
-            throws DBException {
+    public String saveChanges(@RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname, @RequestParam("currentPassword") String currentPassword, @RequestParam("email") String newEmail, @RequestParam("newPassword") String newPassword, Model model, @NotNull HttpSession session) throws DBException {
 
         String oldEmail = ((User) session.getAttribute("user")).getEmail();
         User oldInfo = userController.findUser(oldEmail);
@@ -92,9 +84,7 @@ public class AccountController {
                 String securedPassword = map.get("password");
                 String newSalt = map.get("salt");
 
-                User user =
-                        userController.updateUserInDB(
-                                oldInfo, firstname, lastname, newEmail, securedPassword, newSalt);
+                User user = userController.updateUserInDB(oldInfo, firstname, lastname, newEmail, securedPassword, newSalt);
 
                 session.removeAttribute("user");
                 session.setAttribute("user", user);
@@ -107,21 +97,15 @@ public class AccountController {
 
             return "redirect:/profile";
 
-        } else if (newEmail.isBlank()
-                || newPassword.isBlank()
-                || firstname.isBlank()
-                || lastname.isBlank()
-                || currentPassword.isBlank()) {
+        } else if (newEmail.isBlank() || newPassword.isBlank() || firstname.isBlank() || lastname.isBlank() || currentPassword.isBlank()) {
             LOGGER.info("User did not fill out all fields");
             model.addAttribute("error", "Please fill out all fields.");
-            utils.fillUpInfoOnPage(
-                    model, oldInfo.getFirstName(), oldInfo.getLastName(), oldInfo.getEmail());
+            utils.fillUpInfoOnPage(model, oldInfo.getFirstName(), oldInfo.getLastName(), oldInfo.getEmail());
             return null;
 
         } else {
             model.addAttribute("error", "Please enter correct password.");
-            utils.fillUpInfoOnPage(
-                    model, oldInfo.getFirstName(), oldInfo.getLastName(), oldInfo.getEmail());
+            utils.fillUpInfoOnPage(model, oldInfo.getFirstName(), oldInfo.getLastName(), oldInfo.getEmail());
             return null;
         }
     }
@@ -136,9 +120,7 @@ public class AccountController {
      * @throws DBException we are connecting to the database to delete the account.
      */
     @GetMapping("/removeaccount")
-    public String deleteAccount(
-            @RequestParam("password") String password, Model model, @NotNull HttpSession session)
-            throws DBException {
+    public String deleteAccount(@RequestParam("password") String password, Model model, @NotNull HttpSession session) throws DBException {
 
         String email = ((User) session.getAttribute("user")).getEmail();
         User user = userController.findUser(email);
@@ -182,4 +164,5 @@ public class AccountController {
     public String goToDeleteAccount() {
         return null;
     }
+
 }
